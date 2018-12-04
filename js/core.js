526221b2aea8c5a8dbef8e4eb776e6220baccc41
boberstarosta <boberstarosta@gmail.com>
2018-12-04 10:40:39 +0100

    cookie test

---
+++ js/core.js
@@ -0,0 +1,43 @@
+"use strict";
+
+
+let data = {
+    name: "Example",
+    description: "This is just some data.",
+    values: {
+        visible: true,
+        qty: 5,
+        price: 4.49,
+    },
+};
+
+
+function save() {
+    console.log("saving data");
+    
+    let json = JSON.stringify(data);
+    let payload = encodeURI(json);
+    let cookieStr = "data=" + payload;
+    
+    console.log(cookieStr);
+    
+    document.cookie = cookieStr;
+}
+
+function load() {
+    console.log("loading data");
+    
+    let cookieStr = document.cookie;
+    let decodedJson = decodeURI(cookieStr);
+    let readData = JSON.parse(decodedJson);
+    
+    console.log(readData);
+    
+    $("#output").text(JSON.stringify(readData));
+}
+
+
+$(function() {
+    $("#input").text(JSON.stringify(data))
+});
+
